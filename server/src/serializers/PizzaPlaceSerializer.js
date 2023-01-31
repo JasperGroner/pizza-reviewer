787b59414cbd2755a80d668f7ea72370b554247c
MtJakhar <thvsd.10fingers@gmail.com>
2023-01-31 14:17:25 -0500

    built out front-end for PizzaPlaceList

--- server/src/serializers/PizzaPlaceSerializer.js
+++ server/src/serializers/PizzaPlaceSerializer.js
@@ -2,7 +2,7 @@ import Serializer from "./Serializer.js";
 
 class PizzaPlaceSerializer extends Serializer {
     static getSummary(pizzaPlace) {
-        return this.serialize(pizzaPlace, ["id", "name"])
+        return this.serialize(pizzaPlace, ["id", "name", "imageUrl"])
     }
 }
 
