f682520921c3650e78f4af38393a7a7d2ffa9a5d
Jasper Groner <jasper.groner@gmail.com>
2023-02-01 09:59:41 -0500

    added route for /:id and serializer for pizza-place detail

--- server/src/routes/api/v1/pizzaPlaceRouter.js
+++ server/src/routes/api/v1/pizzaPlaceRouter.js
@@ -14,4 +14,15 @@ pizzaPlaceRouter.get("/", async (req, res) => {
     }
 })
 
+pizzaPlaceRouter.get("/:id", async (req, res) => {
+    const pizzaId = req.params.id
+    try {
+        const pizzaPlace = await PizzaPlace.query().findById(pizzaId)
+        const serializedPizzaPlace = PizzaPlaceSerializer.getDetail(pizzaPlace)
+        return res.status(200).json({pizzaPlace: serializedPizzaPlace})
+    } catch(error) {
+        return res.status(500).json({errors: error})
+    }
+})
+
 export default pizzaPlaceRouter
