7859be7b04f3bc7be95e0ca4803f8d45134748fd
Jasper Groner <jasper.groner@gmail.com>
2023-02-02 11:09:20 -0500

    add route for posting new reviews

---
+++ server/src/routes/api/v1/pizzaPlaceReviewRouter.js
@@ -0,0 +1,24 @@
+import express from "express"
+import { ValidationError } from "objection"
+
+import { PizzaPlace, Review } from "../../../models/index.js"
+import cleanUserInput from "../../../services/cleanUserInput.js"
+
+const pizzaPlaceReviewRouter = new express.Router({ mergeParams: true })
+
+pizzaPlaceReviewRouter.post("/", async (req, res) => {
+  const body = req.body
+  body.pizzaPlaceId = req.params.id
+  const formInput = cleanUserInput(body)
+  try {
+    const newPizzaReview = await PizzaPlace.query().insertAndFetch(formInput)
+    return res.status(201).json({newPizzaReview})
+  } catch(error) {
+    if(error instanceof ValidationError) {
+      return res.status(422).json({ errors: error.data })
+    }
+    return res.status(500).json({ errors: error })
+  }
+})
+
+export default pizzaPlaceReviewRouter
