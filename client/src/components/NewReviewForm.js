640964dab775d013def148fef0d755a096927708
Jasper Groner <jasper.groner@gmail.com>
2023-02-03 15:16:34 -0500

    bug-fix-2

--- client/src/components/NewReviewForm.js
+++ client/src/components/NewReviewForm.js
@@ -9,10 +9,11 @@ const NewReviewForm = ({ pizzaPlace, setPizzaPlace }) => {
     text: ""
   })
 
+  const pizzaId = useParams().id
+
   const [errors, setErrors] = useState({})
 
   const postNewReview = async(newReviewData) => {
-    const pizzaId = useParams().id
     try {
       const response = await fetch(`/api/v1/pizza-places/${pizzaId}/reviews/`, {
         method: "POST",
