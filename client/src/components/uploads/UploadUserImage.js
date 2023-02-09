23d22183ae896eb0907f08a898c099bc8471549d
Jasper Groner <jasper.groner@gmail.com>
2023-02-09 16:45:46 -0500

    Refactor RegistrationForm to eliminate unnecessary <form> tags and to put image upload before register button

--- client/src/components/uploads/UploadUserImage.js
+++ client/src/components/uploads/UploadUserImage.js
@@ -16,23 +16,19 @@ const UploadUserImage = ({ userPayload, setUserPayload }) =>{
 	return (
 		<>
 			<h5>Profile Image Uploads</h5>
-
-			<form className="load primary">
-
-				<Dropzone onDrop={handleImageUpload}>
-					{({getRootProps, getInputProps})=>(
-						<section>
-							<div {...getRootProps()}>
-								<input {...getInputProps()} />
-								<p>Upload your profile Image- drag 'n' drop or click to upload</p>
-							</div>
-						</section>
-					)}
-				</Dropzone>
-				<p>
-					Preview: <img src={source} />
-				</p>
-			</form>
+			<Dropzone onDrop={handleImageUpload}>
+				{({getRootProps, getInputProps})=>(
+					<section>
+						<div {...getRootProps()}>
+							<input {...getInputProps()} />
+							<p>Upload your profile Image- drag 'n' drop or click to upload</p>
+						</div>
+					</section>
+				)}
+			</Dropzone>
+			<p>
+				Preview: <img src={source} />
+			</p>
 		</>
 	)
 }
